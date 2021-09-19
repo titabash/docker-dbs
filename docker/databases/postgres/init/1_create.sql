57579ff466f710182ac1fbd025bc568af9a3737d
titabash <titabash0819@gmail.com>
2021-09-20 00:57:16 +0900

    sqldbの初期化設定を追加

--- docker/databases/postgres/init/1_create.sql
+++ docker/databases/postgres/init/1_create.sql
@@ -1,2 +1,21 @@
-/* DATABASEを作成 */
+/* DATABASEを初期化 */
+create role testuser login password 'password';
 CREATE DATABASE pgtest;
+grant all privileges on database pgtest to user1;
+
+\c pgtest;
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+create table testtbl
+(
+  id SERIAL NOT NULL,
+  name varchar,
+  salary int8,
+  PRIMARY KEY (id)
+);
